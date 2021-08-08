eef48c5d63803c861c2f31590d891a9edcf1a729
Ofri Shani <ofrishani10@gmail.com>
2021-08-08 14:10:15 +0300

    import machine learning

--- my-app/src/Pages/HomePage.js
+++ my-app/src/Pages/HomePage.js
@@ -1,12 +1,16 @@
 import React from 'react';
+// import { Form } from 'semantic-ui-react';
 import Feature from '../Components/Features/Feature';
-
+import './HomePage.css'
 export const HomePage = () => {
     // const {todo, setTodo} = useState([])
    
     return(
-        <div>
+        <div className='form'>
             <Feature text="Scan Profile"/>
+            <Feature text="Scan post"/>
+            <Feature text="about"/>
+            <Feature text="contact"/>
         </div>    
     )
 }
