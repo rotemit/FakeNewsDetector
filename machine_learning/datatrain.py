e5bdb56522d4870b1383a52b8bf8b25ea323e4ee
rotemit <rotem.mitrany@gmail.com>
2021-09-25 17:02:16 +0300

    post mid-report. better text cleaning, added lemmatization, tried regression unsuccessfully

--- machine_learning/datatrain.py
+++ machine_learning/datatrain.py
@@ -17,8 +17,12 @@ from sklearn.feature_extraction.text import HashingVectorizer
 from deep_translator import GoogleTranslator #pip installed
 from heb_data_collector import get_group_posts
 from sklearn.linear_model import LogisticRegressionCV
+from nltk.stem import PorterStemmer
+from nltk.stem import LancasterStemmer
+from nltk.stem import WordNetLemmatizer
+# import nltk
+# nltk.download('wordnet')
 
-#bla
 def grade_single_post(post):
     #load trained model and fitted vectorizer
     svm_model = joblib.load('combined_trained_model.pkl')
@@ -26,11 +30,12 @@ def grade_single_post(post):
     #translate post to english, regardless of source language
     translator = GoogleTranslator()
     translated_post = translator.translate(post)
+    readied_post = readify_text(translated_post)
     #vectorize and predict fakeness
-    vectorized_post = vectorizer.transform([translated_post])
+    vectorized_post = vectorizer.transform([readied_post])
     y_pred = svm_model.predict(vectorized_post)
     #print result
-    print("post:\n"+ post + "\ntranslated post:\n" + translated_post + "\ngrade: " + str(y_pred))
+    print("post:\n" + post + "\ntranslated post:\n" + translated_post + "\nreadied post:\n"+readied_post+"\ngrade: " + str(y_pred))
 
 '''
     manual tests for svm
@@ -55,6 +60,8 @@ def grade_post (post, machine, classifer):
     Remove punctuation
 """
 def clean_txt (txt):
+    #make lowercase
+    txt = txt.lower()
     # initializing punctuations string
     punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
 
@@ -75,20 +82,39 @@ def remove_stopwords(txt):
     result = ' '.join(resultwords)
     return result
 
+def our_stemmer(txt):
+    # ps = PorterStemmer()
+    # toreturn = ' '.join(ps.stem(word) for word in txt.split())
+    # return toreturn
+    ls = LancasterStemmer()
+    toreturn = ' '.join(ls.stem(word) for word in txt.split())
+    return toreturn
+
+def our_lemmatizer(txt):
+    wordnet_lemmatizer = WordNetLemmatizer()
+    toreturn = ' '.join(wordnet_lemmatizer.lemmatize(word, pos="v") for word in txt.split())
+    return toreturn
+
 def readify_text(txt):
     txt = clean_txt(txt)
     txt = remove_stopwords(txt)
+    # txt = our_stemmer(txt)
+    txt = our_lemmatizer(txt)
     return txt
 
 def our_clf(tfidf_train, label_train, tfidf_valid, label_valid, model_filename, vectorizer, vectorizer_filename):
     clf_model = LogisticRegressionCV(cv=5, scoring='accuracy', random_state=0, n_jobs=-1, verbose=3, max_iter=300).fit(
         tfidf_train, label_train)
     # predict the labels for the text validation data
-    label_prediction = clf_model.predict(tfidf_valid)
+    # label_prediction = clf_model.score(tfidf_valid)
 
     # check model accuracy
-    print("Accuracy:", metrics.accuracy_score(label_valid, label_prediction))
-    print(confusion_matrix(label_valid, label_prediction))
+    #try:
+    #    score = reg.score(label_valid, label_prediction)
+    #    print(score)
+    #except:
+    #    print("Accuracy:", metrics.accuracy_score(label_valid, label_prediction))
+    #print(score)
 
     # save trained model
     joblib.dump(clf_model, model_filename)
@@ -119,7 +145,7 @@ def our_svm(tfidf_train, label_train, tfidf_valid, label_valid, model_filename, 
     #check model accuracy
     print("Accuracy:", metrics.accuracy_score(label_valid, label_prediction))
     print(confusion_matrix(label_valid, label_prediction))
-
+#bla
     #save trained model
     joblib.dump(svm_model, model_filename)
 
@@ -138,14 +164,16 @@ def load_trained_svm_model(filename):
 '''
     Clean text:
     1. Remove links
-    2. Replace covid-19/coronavirus with 'Corona', as hebrew speakers write 
+    2. Readify text (clean, remove stopwords, stem)
+    3. Replace covid-19/coronavirus with 'Corona', as hebrew speakers write 
 '''
 def clean_text(txt):
     txt = str(txt)
     ret = ' '.join(item for item in txt.split() if ((not (item.startswith('https://'))) and (not '.com' in item)))
-    ret = ret.replace('COVID-19', 'Corona')
-    ret = ret.replace('Covid-19', 'Corona')
-    ret = ret.replace('Coronavirus', 'Corona')
+    ret = readify_text(ret)
+    ret = ret.replace('covid19', 'corona')
+    ret = ret.replace('coronavirus', 'corona')
+    ret = ret.replace('coronavir', 'corona')
     return ret
 
 '''
@@ -161,21 +189,22 @@ def csv_cleaner(file):
     new_name = file.rsplit(".", 1)[0] + 'Clean.csv'
     df.to_csv(new_name, encoding='utf-8', index=False, mode='w+')
 
-if __name__ == '__main__':
+def clean_datasets():
+    csv_cleaner('trueNews.csv')
+    csv_cleaner('fakeNews.csv')
+    csv_cleaner('Constraint_Train.csv')
+    csv_cleaner('Constraint_Val.csv')
+    csv_cleaner('english_test_with_labels.csv')
+    csv_cleaner('corona_fake.csv')
+
+def training_process():
     #********************** COMBINED DAASETS **********************************
-    # csv_cleaner('trueNews.csv')     #create a new and clean csv file. uncomment only when file changes
-    # csv_cleaner('fakeNews.csv')     #create a new and clean csv file
-    # csv_cleaner('Constraint_Train.csv')
-    # csv_cleaner('Constraint_Val.csv')
-    # csv_cleaner('english_test_with_labels.csv')
-    # csv_cleaner('corona_fake.csv')
     df_true = pd.read_csv('trueNewsClean.csv')
     df_false = pd.read_csv('fakeNewsClean.csv')
     df_Constraint = pd.read_csv('Constraint_TrainClean.csv')
     df_ConstraintVal = pd.read_csv('Constraint_ValClean.csv')
     df_en_test = pd.read_csv('english_test_with_labelsClean.csv')
     df_corona_fake = pd.read_csv('corona_fakeClean.csv')
-    # df_false['our_labels'] = df_false.apply(lambda col: col['Poynter_Label'].upper(), axis=1)
     df_true.dropna(inplace=True)
     df_false.dropna(inplace=True)
     df_Constraint.dropna(inplace=True)
@@ -185,35 +214,29 @@ if __name__ == '__main__':
     frames = [df_true, df_false, df_Constraint, df_ConstraintVal, df_en_test, df_corona_fake]
     df = pd.concat(frames, join='inner')
     df.drop_duplicates()
-
     labels = df['Binary Label']
-
     #***************************************************************************
-
-
     text_train, text_valid, label_train, label_valid = train_test_split(df['clean_text'], labels, test_size=0.2,
                                                                         random_state=109)
     tfidf_vectorizer = TfidfVectorizer(stop_words='english', strip_accents='unicode', ngram_range=(1, 2), norm=None)
     tfidf_train = tfidf_vectorizer.fit_transform(text_train)
     tfidf_valid = tfidf_vectorizer.transform(text_valid)
     svm_classifier = our_svm(tfidf_train, label_train, tfidf_valid, label_valid, 'combined_trained_model.pkl', tfidf_vectorizer, 'tfidf_vectorizer.pkl')
+    #clf_classifier = our_clf(tfidf_train, label_train, tfidf_valid, label_valid, 'CLF_combined_trained_model.pkl', tfidf_vectorizer, 'CLF_tfidf_vectorizer.pkl')
+    our_svm_tests(tfidf_vectorizer, svm_classifier)
 
-    clf_classifier = our_clf(tfidf_train, label_train, tfidf_valid, label_valid, 'combined_trained_model.pkl', tfidf_vectorizer, 'tfidf_vectorizer.pkl')
-
-    our_svm_tests(tfidf_vectorizer, clf_classifier)
-
-
-
-    # *************** FINISH **********************
-    # load_trained_svm_model('combined_trained_model.pkl')
-    # grade_single_post('"קבלו רמז: אני לא התחסנתי בכלל ולא נדבקתי ולא הייתי חולה. ביי "')
-    # grade_single_post("חבל כל חיסון מגביר את הסיכוי להידבק עוד הפעם")
-    # grade_single_post("שאלה בורה, החיסון ידוע לכל אחד שלא מונע הדבקה")
-    # grade_single_post("השגרירות ההודית בטוקיו אמרה שיותר מחבר צוות הודי אחד על נסיכת היהלום נמצא חיובי לקורונה")
-    # grade_single_post("Just in: Novel coronavirus named 'Corona': UN health agency. (AFP)")
-    # grade_single_post("WHO officially names coronavirus as Corona. CoronaOutbreak")
-    # grade_single_post("The Indian Embassy in Tokyo has said that one more Indian crew member on Diamond Princess has tested positive for Corona.")
-    # grade_single_post("קורונה גורמת לתחלואה קשה ותמותה גם בקרב צעירים שאינם מחוסנים 📈 עד גיל 50 - מי שלא התחסנו כלל, מהווים 69% מהחולים קשה ו-96% מהנפטרים (בין התאריכים 1.6 ל-4.9). אלו העובדות. חיסון ראשון, שני או שלישי - פשוט צאו להתחסן!")
+if __name__ == '__main__':
+    #clean_datasets()  # uncomment when datasets change, or when cleaning process changes
+    training_process()  #uncomment when we want to redo training
+    # *************** MANUAL CHECKS **********************
+    grade_single_post('"קבלו רמז: אני לא התחסנתי בכלל ולא נדבקתי ולא הייתי חולה. ביי "')
+    grade_single_post("חבל כל חיסון מגביר את הסיכוי להידבק עוד הפעם")
+    grade_single_post("שאלה בורה, החיסון ידוע לכל אחד שלא מונע הדבקה")
+    grade_single_post("השגרירות ההודית בטוקיו אמרה שיותר מחבר צוות הודי אחד על נסיכת היהלום נמצא חיובי לקורונה")
+    grade_single_post("Just in: Novel coronavirus named 'Corona': UN health agency. (AFP)")
+    grade_single_post("WHO officially names coronavirus as Corona. CoronaOutbreak")
+    grade_single_post("The Indian Embassy in Tokyo has said that one more Indian crew member on Diamond Princess has tested positive for Corona.")
+    grade_single_post("קורונה גורמת לתחלואה קשה ותמותה גם בקרב צעירים שאינם מחוסנים 📈 עד גיל 50 - מי שלא התחסנו כלל, מהווים 69% מהחולים קשה ו-96% מהנפטרים (בין התאריכים 1.6 ל-4.9). אלו העובדות. חיסון ראשון, שני או שלישי - פשוט צאו להתחסן!")
     grade_single_post("קורונה זו לא רק 'מחלה של מבוגרים'! גם צעירים שלא מתחסנים חושפים עצמם למחלה קשה. 85% ממאושפזי הקורונה אשר נמצאים כעת במצב קריטי ומחוברים למכשיר אקמו - אינם מחוסנים. גילם הממוצע - 47. קורונה עלולה להיות מחלה קשה למבוגרים ולצעירים אבל בעיקר ללא מחוסנים. צאו להתחסן.")
     grade_single_post("עשרות, מאות, וכנראה אלפי אנשים שהוזרקו, מתים סובלים מדלקות בלב מנכויות קשות, מדום לב חולים במחלה שהתחסנו ממנה הכל על פי עדויות ממקור ראשון שנאספות בקושי ומראות רק את קצה הקרחון בעוד שהתמונה האמיתית נשארת במחשכים, מצונזרת ומוסתרת באלימות פראית ")
 
