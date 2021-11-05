af23a1b272dd6b8c6f51952197e631ca10de19e9
MaayanSh97 <maayan581997@gmail.com>
2021-11-05 15:11:04 +0200

    datatrain

--- modules/Scan_result.py
+++ modules/Scan_result.py
@@ -18,3 +18,7 @@ class ScanResult:
     def __str__(self):
         return "Name: " + self.user_name + "\nSentiment Analyzer Result:\n" + str(self.sentimentAnalyzer_result) + \
                "\nMachine Learning Result:\n" + str(self.machineLearning_result)+ "\nTrust Value Result:\n" + str(self.utv_result)
+
+
+    def __dict__(self):
+        return {"user_name":self.user_name, "sentimentAnalyzer_result":vars(self.sentimentAnalyzer_result), "machineLearning_result":vars(self.machineLearning_result), "utv_result":vars(self.utv_result)}
